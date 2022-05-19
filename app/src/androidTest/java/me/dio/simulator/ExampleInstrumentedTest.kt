030a36881f43f5c4f965638ab7616220a608b3b1
henrique2m <btdavintehenrique@gmail.com>
2022-05-18 22:21:23 -0300

    :sparkles: end  :rocket:

--- app/src/androidTest/java/me/dio/simulator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/me/dio/simulator/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package simulator.com
+package me.dio.simulator
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("simulator.com", appContext.packageName)
+        assertEquals("me.dio.simulator", appContext.packageName)
     }
 }
--- app/src/test/java/me/dio/simulator/ExampleUnitTest.kt
+++ app/src/test/java/me/dio/simulator/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package simulator.com
+package me.dio.simulator
 
 import org.junit.Test
 
